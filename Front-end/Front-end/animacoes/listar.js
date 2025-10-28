d069bc2bc23684a39d36ddbec269b48d8c4b69b5
Yago-stack <fabricioruf@gmail.com>
2025-10-28 09:31:17 -0300

    .
    .

--- Front-end/Front-end/animacoes/listar.js
+++ Front-end/Front-end/animacoes/listar.js
@@ -4,5 +4,39 @@ function buscaPagamentos(){
     .then (res=> res.json())
     .then (dados => {
         console.log(dados)
+        exibePagamentos(dados);
+
     })
+}
+
+let tabelaPagamentos = document.getElementById("tabelaPagamentos")
+
+//função para retornar ao filtrar o tipo:entrada ou saida
+function exibePagamentos(dados){
+    let pagamento = dados[0]
+    //cria os elementos da tabela
+    let linhaPagamento = document.createElement("tr")
+    let tipoPagamentoTd= document.createElement("td")
+    let nomeCliente= document.createElement("td")
+    let descricao= document.createElement("td")
+    let data= document.createElement("td")
+    let valor= document.createElement("td")
+    //adiciona as informações nas tag em HTML a partir do JSON retornado da API
+    tipoPagamentoTd.textContent= pagamento.tipo
+    nomeCliente.textContent= pagamento.nomeCliente
+    descricao.textContent= pagamento.descricao
+    data.textContent= pagamento.dataPagamento
+    valor.textContent= "R$" + parseFloat(pagamento.valor).toFixed(2)
+
+    //informa que os TD são filhos do TR
+    linhaPagamento.appendChild(tipoPagamentoTd)
+    linhaPagamento.appendChild(nomeCliente)
+    linhaPagamento.appendChild(descricao)
+    linhaPagamento.appendChild(data)
+    linhaPagamento.appendChild(valor)
+
+    //informa que o TR é filho da tabela
+    tabelaPagamentos.appendChild(linhaPagamento)
+
+
 }
